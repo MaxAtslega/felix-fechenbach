e7f20605967700fca170bb71fbf701018509f17a
MaxAtslega <max.atslega@hotmail.com>
2023-02-15 13:08:46 +0100

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'de'
+  },
+  compiler: {
+    styledComponents: {
+      displayName: false
+    }
   }
 }
 
