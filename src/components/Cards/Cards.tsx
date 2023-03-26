5ac6dc93893052c1def80d870a410a67f1527ce7
MaxAtslega <max.atslega@hotmail.com>
2023-03-26 12:12:01 +0200

    Change text

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -4,19 +4,16 @@ import { FormattedMessage } from 'react-intl'
 interface Props {
   title: string
   description: string
-  index: number
   slug: string
   image: string
 }
 
-const Card = ({ index, title, description, slug, image }: Props) => {
+const Card = ({ title, description, slug, image }: Props) => {
   return (
     <Item>
       <CardContainer>
         <Header>
-          <span>
-            {index} - {title}
-          </span>
+          <span>{title}</span>
         </Header>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={image} alt={'title'} />
